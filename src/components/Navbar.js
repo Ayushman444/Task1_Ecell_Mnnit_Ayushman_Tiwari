271b8d07a883787c090675dd6af5a2a20b46579e
Ayushman444 <ayushman5022@gmail.com>
2023-09-17 02:51:54 +0530

    Website Hosted on FireBase

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,31 +20,33 @@ export const Navbar = ({onSelectComponent}) => {
         setMenuOpen(!menuOpen);
     };
     return (
-        <div className='border-b-4 border-gray-300'>
-          <div className='flex flex-row justify-end items-center mr-5'>
-            {menuOpen ? (
-              <div className='md:hidden'>
-                <button onClick={toggleMenu} className='text-2xl focus:outline-none'>
-                  <FaTimes />
-                </button>
-              </div>
-            ) : (
-              <div className='md:hidden'>
-                <button onClick={toggleMenu} className='text-2xl focus:outline-none'>
-                  <FaBars />
-                </button>
-              </div>
-            )}
-            <div className={`md:flex ${menuOpen ? 'flex flex-col' : 'hidden'} md:space-x-4`}>
-              <button className='border-none m-2 font-bold' onClick={()=>handleButtonClick('second')}>Home</button>
-              <button className='border-none m-2 font-bold' onClick={()=>handleButtonClick('main')}>Grid</button>
-              <a href="https://www.linkedin.com/in/ayushman-tiwari-a7807b243/">
-              <button className='border-none m-2 font-bold' >LinkedIn</button>
-              </a>
-              
-            </div>
-          </div>
+      <div className='border-b-4 border-gray-300'>
+      <div className={`flex flex-row justify-${menuOpen ? 'center' : 'end'} items-center md:justify-end md:items-center md:mr-5`}>
+        <div className={`flex justify-center items-center md:flex ${menuOpen ? 'flex-row' : 'hidden'} md:space-x-4`}>
+          <button className='border-none m-2 font-bold' onClick={() => handleButtonClick('second')}>Home</button>
+          <button className='border-none m-2 font-bold' onClick={() => handleButtonClick('main')}>Grid</button>
+          <a href="https://www.linkedin.com/in/ayushman-tiwari-a7807b243/">
+            <button className='border-none m-2 font-bold'>LinkedIn</button>
+          </a>
         </div>
+
+        {menuOpen ? (
+          <div className='md:hidden ml-auto'>
+            {/* Add ml-auto class to move the button to the right */}
+            <button onClick={toggleMenu} className='text-2xl focus:outline-none'>
+              <FaTimes />
+            </button>
+          </div>
+        ) : (
+          <div className='md:hidden ml-auto'>
+            {/* Add ml-auto class to move the button to the right */}
+            <button onClick={toggleMenu} className='text-2xl focus:outline-none'>
+              <FaBars />
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
       );
       
 }
